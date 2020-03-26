7e95ad12e96eac58c02db3ced6decfe81fdd0af9
unknown <winnie.joe123@gmail.com>
2020-03-26 12:18:29 -0600

    finish activity 1

--- src/CardTester.java
+++ src/CardTester.java
@@ -8,6 +8,9 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card card = new Card("seven", "hearts", 7);
+		System.out.println(card.toString());
+		Card card2 = new Card("five", "hearts", 5);
+		System.out.println(card.matches(card2));
 	}
 }
